38849e0246f4cfd8c9484f3cfa54ebbc79a27625
Eirren <edv2019@gmail.com>
2026-04-24 14:41:56 -0700

    Milestone 5 completed, created loop that supports lanes and probabilities, including lane shifting

--- Lab33.cpp
+++ Lab33.cpp
@@ -8,6 +8,9 @@
 using namespace std;
 
 const int MAX_TIME = 20; // time periods
+const int PAY_LEAVE = 46; // paying and leaving probability
+const int JOIN_QUEUE = 85; // joining queue probability
+const int EMPTY_QUEUE = 50; // if queue empty, 50/50 probability of joining
 
 void printQueue(deque<Car> carQ); // prototype for printing car queue function
 
@@ -28,6 +31,7 @@ int main() {
     }
 
     int random;
+    int laneShift = 0; // lane to shift to
     int count = 1;
     int lane = 1;
 
@@ -39,43 +43,46 @@ int main() {
         for (int j = 0; j < tollBooth.size(); ++j) {
             random = rand() % 100; // randomize probability for each lane
             cout << "Lane: " << lane;
-            if (random <= 50 && !tollBooth.at(j).empty()) { // if car paid and lane is not empty
+            if (tollBooth.at(j).empty()) { // if lane empty, 50/50 new car enters queue
+                if (random <= EMPTY_QUEUE) {
+                    cout << " Joined: ";
+                    tollBooth.at(j).push_back(Car());
+                    tollBooth.at(j).back().print();
+                }
+                else {
+                    cout << " No cars joined. " << endl;
+                }
+            }    
+            else if (random <= PAY_LEAVE) { // if car paid
                 cout << " Paid: ";
                 tollBooth.at(j).front().print();
                 tollBooth.at(j).pop_front();
             }
-            else { // car joined
+            else if (random <= JOIN_QUEUE){ // car joined
                 cout << " Joined: ";
                 tollBooth.at(j).push_back(Car());
                 tollBooth.at(j).back().print();
             }
+            else { // rear car shifts lanes
+                if (!tollBooth.at(j).empty()) {
+                    while (laneShift == j) { // get random lane other than current lane
+                        laneShift = rand() % 4;
+                    }
+                    Car tmpCar = tollBooth.at(j).back();
+                    cout << " Switched: ";
+                    tmpCar.print();
+                    tollBooth.at(laneShift).push_back(tmpCar);
+                    tollBooth.at(j).pop_back();
+                }
+            }
             ++lane;
         }
         for (int i = 0; i < tollBooth.size(); ++i) { // print queues
-            cout << "Lane " << i + 1 << ":" << endl;
+            cout << "Lane " << i + 1 << " Queue:" << endl;
             printQueue(tollBooth.at(i));
         }
         ++count;
     }
-
-    /* commenting out to test array
-    while(!carQueue.empty()) {
-        
-        cout << "Time: " << count << " Operation: ";
-        if (random <= 45) {
-            cout << " Joined lane: ";
-            carQueue.push_back(Car());
-            carQueue.back().print();
-        }
-        else {
-            cout << " Car paid: ";
-            carQueue.front().print();
-            carQueue.pop_front();
-        }
-        printQueue(carQueue);
-        ++count;
-    }
-    */
 
     return 0;
 }
