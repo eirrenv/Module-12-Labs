47d529d04cba05f43ec2b7d6d1b9f6ad180a746c
Eirren <edv2019@gmail.com>
2026-04-24 14:13:46 -0700

    Created deque  array for toll booth, created and tested initial array values

--- Lab33.cpp
+++ Lab33.cpp
@@ -2,6 +2,7 @@
 #include <deque>
 #include <random> // probability
 #include <string>
+#include <array>
 #include <cstdlib>
 #include "Car.h"
 using namespace std;
@@ -11,11 +12,20 @@ void printQueue(deque<Car> carQ); // prototype for printing car queue function
 int main() {
     srand(time(0));
 
-    deque<Car> carQueue(2); // create deque for car list
+    array<deque<Car>, 4> tollBooth; // create toll booth array with 4 lanes
+
+    for (int i = 0; i < tollBooth.size(); ++i) {
+        tollBooth.at(i).push_back(Car());
+        tollBooth.at(i).push_back(Car());
+    }
 
     cout << "Initial queue:" << endl;
-    printQueue(carQueue);
+    for (int i = 0; i < tollBooth.size(); ++i) {
+        cout << "Lane " << i + i << ":" << endl;
+        printQueue(tollBooth.at(i));
+    }
 
+    /* commenting out to test array
     int random;
 
     int count = 1;
@@ -35,6 +45,7 @@ int main() {
         printQueue(carQueue);
         ++count;
     }
+    */
 
     return 0;
 }
