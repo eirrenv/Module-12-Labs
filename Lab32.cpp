45c9a9797bdb20defe61d0f0e0337856c0d845fe
Eirren <edv2019@gmail.com>
2026-04-24 13:45:24 -0700

    Working on loop

--- Lab32.cpp
+++ Lab32.cpp
@@ -1,17 +1,32 @@
 #include <iostream>
 #include <deque>
+#include <random> // probability
+#include <string>
+#include <cstdlib>
 #include "Car.h"
 using namespace std;
 
 void printQueue(deque<Car> carQ); // prototype for printing car queue function
 
 int main() {
+    srand(time(0));
 
     deque<Car> carQueue(2); // create deque for car list
 
     cout << "Initial queue:" << endl;
     printQueue(carQueue);
-    
+
+    int random = rand() % 100;
+    int count = 1;
+    while(!carQueue.empty()) {
+        cout << "Time: " << count << " Operation: ";
+        if (random <= 45) {
+            carQueue.push_back(Car());
+            carQueue.back().print();
+            cout << endl;
+        }
+    }
+
     return 0;
 }
 
