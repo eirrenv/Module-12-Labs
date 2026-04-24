0e87fd9fe1f9f7108f8c84e940d46403197280f8
Eirren <edv2019@gmail.com>
2026-04-24 13:35:18 -0700

    Created printQueue function for easy queue printing

--- Lab32.cpp
+++ Lab32.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
 #include <deque>
-#include <Car.h>
+#include "Car.h"
 using namespace std;
 
+void printQueue(deque<Car> carQ); // prototype for printing car queue function
+
 int main() {
-    deque<Car> carQueue(2);
 
+    deque<Car> carQueue(2); // create deque for car list
 
+    cout << "Initial queue:" << endl;
+    printQueue(carQueue);
+    
     return 0;
+}
+
+void printQueue(deque<Car> carQ) {
+    for (int i = 0; i < carQ.size(); ++i) {
+        cout << "\t";
+        carQ[i].print();
+    }
 }
